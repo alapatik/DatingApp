a1fa5090b80366feaddffdcc7624451fe3cd37b1
Kalyan Alapati <kalyan.alapati@yahoo.co.in>
2019-10-30 13:14:30 +0000

    Logout

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,11 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 model: any = {};
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
   login(){
-    console.log(this.model);
+    this.authService.login(this.model).subscribe((response: any) =>{
+      console.log('Successfully logged in.');
+      }, (error: any) => {
+        console.log('Failed to login. Error: ' + error);
+      });
+  }
+  loggedIn(){
+    let localToken = localStorage.getItem('token');
+    return !!localToken;
+  }
+  logout(){
+    localStorage.removeItem('token');
+    console.log('Loggedout!');
   }
 }
